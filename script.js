8fb663de8e631f8aa567ab0aef5facb4e83136ee
AlexKlim <alex.klimenkov89@gmail.com>
2012-11-20 12:30:38 +0300

    Update script.js
    refactoring style 

--- script.js
+++ script.js
@@ -20,23 +20,23 @@ function main() {
 	  	var key = window.location.pathname.split('/')[2] + "=" + i;
 
 	  	var id = "#" + i;
-			if(items[key] == 1) {
-				$(id).attr('checked', true);
-				upCount();
+		if(items[key] == 1) {
+			$(id).attr('checked', true);
+			upCount();
 
-			} else {
-				$(id).attr('checked', false);
-			}
-
-			setClass($("#issue-description li:eq(" + i + ")").find('input'));
+		} else {
+			$(id).attr('checked', false);
 		}
+
+		setClass($("#issue-description li:eq(" + i + ")").find('input'));
+	}
 
 	});
 
 	for (var i = 0; i < issue_numb_points; i++) {
 	  var id = "#" + i;
 	  $(id).on('click', function() {
-	  	var obj = {};
+	    var obj = {};
 	    var key = window.location.pathname.split('/')[2];
 	    var current_id = $(this).attr('id');
 	    key = key + "=" + current_id;
@@ -45,26 +45,26 @@ function main() {
 	    if(value == 1) {
 
 	    	var inputs = $(this.parentNode).find('input');
-				var counts = 1;
-				for (var j = 0; j < inputs.length; j++) {
-					var _id = inputs[j].id;
+		var counts = 1;
+		for (var j = 0; j < inputs.length; j++) {
+			var _id = inputs[j].id;
 	    		key = window.location.pathname.split('/')[2] + "=" + _id;
 	    		obj[key] = true;
 
-					counts += inputs[j].checked ? 0 : 1;
-		  	}
+			counts += inputs[j].checked ? 0 : 1;
+		}
 
-				inputs.attr('checked', true);
+		inputs.attr('checked', true);
 	    	upCount(counts);
-	    } else {
 
+	    } else {
 	    	obj[key] = value;
 	    	downCount();
 	    }
 
 	    var inputs = $(this.parentNode).find('input');
 	    setClass(inputs);
-			storage.set(obj);
+	    storage.set(obj);
 	  });
 	}
 }
@@ -72,11 +72,11 @@ function main() {
 function showResults(done_items) {
 
 	var rez_text = 	"<div id='results'>Done Items / All Items:&nbsp;" +
-										"<div id='done_items_count'>" +
-											done_items +
-										"</div>" + "&nbsp;/&nbsp;" +
-										issue_numb_points +
-									"</div>";
+				"<div id='done_items_count'>" +
+					done_items +
+				"</div>" + "&nbsp;/&nbsp;" +
+				issue_numb_points +
+			"</div>";
 
 	$('#issue-description').append(rez_text);
 }
@@ -106,14 +106,16 @@ function downCount() {
 }
 
 function setClass(elements) {
-  for (var i = 0; i < elements.length; i++) {
+	for (var i = 0; i < elements.length; i++) {
 
 		var _id = elements[i].id;
 		var parent = $("#" + _id)[0].parentNode;
+
 		if(elements[i].checked) {
 			parent.classList.remove('unchecked');
 			parent.classList.remove('red_color');
 			parent.classList.add('checked');
+
 		} else {
 			parent.classList.remove('checked');
 			parent.classList.add('unchecked');
@@ -121,7 +123,7 @@ function setClass(elements) {
 			if (need_red_color)
 				parent.classList.add('red_color');
 		}
-  }
+  	}
 }
 
 main();
